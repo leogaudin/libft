a0e0588996f162dc7da59c1ab1a76e42efbc4111
Léo <leogaudinoff@gmail.com>
2023-04-12 18:54:33 +0200

    fix(ft_substr): :zap: Fix memory leak

--- ft_substr.c
+++ ft_substr.c
@@ -6,11 +6,21 @@
 /*   By: lgaudin <lgaudin@student.42malaga.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/11 17:08:40 by lgaudin           #+#    #+#             */
-/*   Updated: 2023/04/11 17:23:04 by lgaudin          ###   ########.fr       */
+/*   Updated: 2023/04/12 18:54:26 by lgaudin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
+
+static int	ft_strlen(char *str)
+{
+	int	count;
+
+	count = 0;
+	while (str[count])
+		count++;
+	return (count);
+}
 
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
@@ -18,10 +28,13 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	char	*sub;
 
 	i = 0;
-	sub = malloc(len * sizeof(char));
+	if (ft_strlen(s + start) < len)
+			sub = malloc((ft_strlen(s + start) + 1) * sizeof(char));
+	else
+		sub = malloc((len + 1) * sizeof(char));
 	if (!sub)
 		return (0);
-	while (i < len)
+	while (i < len && s[start + i])
 	{
 		sub[i] = s[start + i];
 		i++;
@@ -29,3 +42,10 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	sub[i] = '\0';
 	return (sub);
 }
+
+// int main(void)
+// {
+// 	char* str = "Salut";
+// 	int start = 2;
+// 	return (0);
+// }
