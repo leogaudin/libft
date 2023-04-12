ba512344389f61647689fec5ad4e7d6da8285aa4
Léo <leogaudinoff@gmail.com>
2023-04-12 18:43:13 +0200

    fix(ft_split): :zap: Fix memory leak

--- ft_split.c
+++ ft_split.c
@@ -6,15 +6,12 @@
 /*   By: lgaudin <lgaudin@student.42malaga.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/16 13:30:01 by lgaudin           #+#    #+#             */
-/*   Updated: 2023/04/12 16:29:21 by lgaudin          ###   ########.fr       */
+/*   Updated: 2023/04/12 18:42:34 by lgaudin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <stdlib.h>
-
-// General max compiler string length
-#define MAX 509
 
 /**
  * @brief
@@ -32,7 +29,7 @@ static int	count_strings(char const *str, char c)
 
 	strings_count = 0;
 	is_char = 0;
-	while (*str)
+	while (*(++str))
 	{
 		if (*str != c && is_char == 0)
 		{
@@ -40,109 +37,81 @@ static int	count_strings(char const *str, char c)
 			strings_count++;
 		}
 		else if (*str == c)
-		{
 			is_char = 0;
-		}
-		str++;
 	}
 	return (strings_count);
 }
 
-static int	get_start(char const *s1, char c)
+static void	*ft_calloc(size_t nelem, size_t elsize)
 {
-	int	i;
+	size_t			i;
+	unsigned char	*pointer;
 
-	i = 0;
-	while (s1[i])
-	{
-		if (s1[i] == c)
-			i++;
-		else
-			break ;
-	}
-	return (i);
-}
-
-static int	get_end(char const *s1, char c)
-{
-	int	i;
-
-	i = 0;
-	while (s1[i])
-		i++;
-	i--;
-	while (i > 0)
-	{
-		if (s1[i] == c)
-			i--;
-		else
-			break ;
-	}
-	i++;
-	return (i);
-}
-
-static char	*ft_strtrim(char const *s1, char c)
-{
-	int		start;
-	int		end;
-	int		i;
-	char	*trimmed;
-
-	start = get_start(s1, c);
-	end = get_end(s1, c);
-	if (start > end)
-		return ((char *) s1);
-	trimmed = malloc((end - start + 1) * sizeof(char));
-	i = 0;
-	if (!trimmed || (start == 0 && end == 0))
+	if (nelem == 0 || elsize == 0)
 		return (0);
+	pointer = malloc(nelem * elsize);
+	i = 0;
+	while (i < (nelem * elsize))
+	{
+		pointer[i] = 0;
+		i++;
+	}
+	return (pointer);
+}
+
+static char	*cut_string(char const *s, int start, int end)
+{
+	int		i;
+	char	*string;
+
+	i = 0;
+	string = ft_calloc((end - start + 1), sizeof(char));
 	while (start < end)
 	{
-		trimmed[i] = s1[start];
-		start++;
+		string[i] = s[start];
 		i++;
+		start++;
 	}
-	return (trimmed);
+	string[i] = 0;
+	return (string);
 }
 
 char	**ft_split(char const *s, char c)
 {
 	char	**result;
 	int		result_index;
-	int		string_index;
 	int		i;
+	int		current_start;
 
-	s = ft_strtrim(s, c);
-	result = malloc(sizeof(char *) * count_strings(s, c));
-	i = 0;
-	while (i < count_strings(s, c))
-		result[i++] = malloc(sizeof(char) * 1000);
+	result = malloc(sizeof(char *) * (count_strings(s, c) + 1));
 	i = -1;
 	result_index = 0;
-	string_index = 0;
+	current_start = -1;
 	while (s[++i])
 	{
-		if (s[i] != c)
-			result[result_index][string_index++] = s[i];
-		else if (s[i + 1] != c)
+		if (s[i] != c && current_start == -1)
+			current_start = i;
+		else if ((s[i] == c || s[i + 1] == 0) && current_start != -1)
 		{
-			result_index++;
-			string_index = 0;
+			if (s[i + 1] == 0)
+				result[result_index++] = cut_string(s, current_start, i + 1);
+			else
+				result[result_index++] = cut_string(s, current_start, i);
+			current_start = -1;
 		}
 	}
-	result[result_index + 1] = 0;
+	result[result_index] = 0;
 	return (result);
 }
 
 // int main(void)
 // {
-// 	char str[] = "     ";
+// 	char *str = "Salut ca va";
 // 	char sep = ' ';
 // 	printf("There are %d strings in \"%s\"\n", count_strings(str, sep), str);
 // 	char **result = ft_split(str, sep);
 // 	int i = 0;
-// 	while (i < count_strings(str, sep))
+// 	while (i <= count_strings(str, sep))
 // 	{
 // 		printf("String %d: %s\n", i, result[i]);
 // 		i++;
