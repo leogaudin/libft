99d6b02c4310ef8ce3f28f36551c4eab9d65210e
Léo <leogaudinoff@gmail.com>
2023-04-13 12:48:46 +0200

    fix(libft.h): :ambulance: Fix wrong prototypes of functions in the header file

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: lgaudin <lgaudin@student.42malaga.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/12 16:50:27 by lgaudin           #+#    #+#             */
-/*   Updated: 2023/04/12 19:44:29 by lgaudin          ###   ########.fr       */
+/*   Updated: 2023/04/13 12:48:10 by lgaudin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,11 @@
 int				ft_atoi(char *str);
 void			ft_bzero(void *s, size_t n);
 void			*ft_calloc(size_t nelem, size_t elsize);
-int				ft_isalnum(char *str);
-int				ft_isalpha(char *str);
-int				ft_isascii(char *str);
-int				ft_isdigit(char *str);
-int				ft_isprint(char *str);
+int				ft_isalnum(int c);
+int				ft_isalpha(int c);
+int				ft_isascii(int c);
+int				ft_isdigit(int c);
+int				ft_isprint(int c);
 char			*ft_itoa(int n);
 void			*ft_memchr(const void *s, int c, size_t n);
 int				ft_memcmp(const void *s1, const void *s2, size_t n);
@@ -38,8 +38,8 @@ char			*ft_strchr(const char *s, int c);
 char			*ft_strdup(char *src);
 void			ft_striteri(char *s, void (*f)(unsigned int, char *));
 char			*ft_strjoin(char const *s1, char const *s2);
-unsigned int	ft_strlcat(char *dest, char *src, unsigned int size);
-unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size);
+size_t			ft_strlcat(char *dest, char *src, size_t size);
+size_t			ft_strlcpy(char *dest, char *src, size_t size);
 int				ft_strlen(char *str);
 char			*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 int				ft_strncmp(char *s1, char *s2, unsigned int n);
