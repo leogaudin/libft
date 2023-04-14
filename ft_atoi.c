94d7703bf9360f97b2647b6eb71b8b51d6f2b22b
Léo <leogaudinoff@gmail.com>
2023-04-14 15:18:50 +0200

    fix(ft_atoi): :art: Fix a comment that was too long for the norminette

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: lgaudin <lgaudin@student.42malaga.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/06 18:00:20 by lgaudin           #+#    #+#             */
-/*   Updated: 2023/04/14 15:16:33 by lgaudin          ###   ########.fr       */
+/*   Updated: 2023/04/14 15:18:19 by lgaudin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,17 +37,3 @@ int	ft_atoi(const char *str)
 	result *= negative;
 	return (result);
 }
-
-// int main() {
-//   const char first[] = "+-54";
-//   const char second[] = "-+48";
-//   const char third[] = "--47";
-//   const char fourth[] = "++47";
-//   const char fifth[] = "47";
-//   printf("First: atoi returned %d and ft_atoi returned %d\n", atoi(first), ft_atoi(first));
-//   printf("second: atoi returned %d and ft_atoi returned %d\n", atoi(second), ft_atoi(second));
-//   printf("third: atoi returned %d and ft_atoi returned %d\n", atoi(third), ft_atoi(third));
-//   printf("fourth: atoi returned %d and ft_atoi returned %d\n", atoi(fourth), ft_atoi(fourth));
-//   printf("fifth: atoi returned %d and ft_atoi returned %d\n", atoi(fifth), ft_atoi(fifth));
-//   return 0;
-// }
